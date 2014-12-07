31e1a6f9369ad1271fdd4a00d2f0d6a81d743339
Ed <eduards.torba@gmail.com>
2014-12-07 00:05:22 +0000

    Refactoring main.js, index.html, css

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,31 +3,33 @@ var socket = io();
 window.onload = function() {
     'use strict';
     
-    // Enable keys flag
+    ///////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
+    //////
+    //////
+    //////      Attributes
+    //////
+    //////
+    ///////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
     var keysFlag;
-    
-    // Keys
     var key = {
-        'space' : 32,
-        'enter' : 13
+        'space': 32,
+        'enter': 13
     };
     
-    // Round count
-    var round = 0;
-    
-    // Timer
     var timer;
+    var round = 0;
+    var word;
     
-    // All frames
     var frames = document.querySelectorAll('.frame-block');
-    
-    // Frame lookup object
     var frame = {
         'home': 0,
         'host': 1,
         'join': 2,
-        'dc'  : 3,
+        'opponentLeft'  : 3,
         'game': 4,
+        'dc': 5,
         'switchTo' : function(id) {
             // Hide all frames
             for(var i = 0; i < frames.length; i++) {
@@ -39,35 +41,34 @@ window.onload = function() {
         }
     };
     
-    // Buttons list
     var button = {
         'frameHomeHost'  : document.getElementById('frameHomeHost'),
         'frameHomeJoin'  : document.getElementById('frameHomeJoin'),
         'frameJoinEnter' : document.getElementById('frameJoinEnter'),
-        'frameDcGoHome'  : document.getElementById('frameDcGoHome')
+        'frameOpponentLeftGoHome' : document.getElementById('frameOpponentLeftGoHome')
     };
     
-    // Score
+    var input = {
+        'word': document.getElementById('player__word'),
+        'code': document.getElementById('frameJoinCode')
+    };
+    
     var score = {
-        'lost'   : document.getElementById('player__top--score'),
-        'won'    : document.getElementById('player__bottom--score'),
+        'lost'   : document.getElementById('player--top-score'),
+        'won'    : document.getElementById('player--bottom-score'),
         'update' : function() {
             score.won.innerHTML = 'Won ' + score.won.dataset.duelsWon + ' duels!';
             score.lost.innerHTML = 'Won ' + score.lost.dataset.duelsLost + ' duels!';
         }
     };
     
-    // Message
-    var messages = {
-        'message' : document.getElementById('game__message')
+    var message = {
+        'game': document.getElementById('game__message'),
+        'code': document.getElementById('frameHostCode'),
+        'set': function(elem, msg) {
+            elem.innerHTML = msg;
+        }
     };
-    
-    // Input fields
-    var inputs = {
-        'playerWord' : document.getElementById('player__word')
-    };
-    
-    var word;
     
     ///////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////
@@ -81,10 +82,10 @@ window.onload = function() {
     
     button.frameHomeHost.onclick = function() {
         // Generate code
-        var code = randomCode(8, '#aA');
+        var code = randomCode(5, '#aA');
         
         // Place game code on the second frame
-        document.getElementById('frameHostCode').innerHTML = code;
+        message.set(message.code, code);
         
         // Emit to app.js
         socket.emit('joinRoom', code);
@@ -122,7 +123,7 @@ window.onload = function() {
     
     button.frameJoinEnter.onclick = function() {
         // Get the code from the input field
-        var code = document.getElementById('frameJoinCode').value;
+        var code = input.code.value;
         
         if (code.length == 0) {
             alert('Please enter code');
@@ -138,7 +139,7 @@ window.onload = function() {
     ///////////////////////////////////////////////////////////////////
     //////
     //////
-    //////      Both Players Joined Event
+    //////      Both Players Joined - switch to game frame
     //////
     //////
     ///////////////////////////////////////////////////////////////////
@@ -160,26 +161,17 @@ window.onload = function() {
     ///////////////////////////////////////////////////////////////////
     
     socket.on('opponentDisconnected', function() {
-        // Reset local attributes e.g. round counter etc.
-        clearInterval(timer);
-        keysFlag = false;
-        round = 0;
-        score.won.dataset.duelsWon = 0;
-        score.lost.dataset.duelsLost = 0;
-        score.update();
-        inputs.playerWord.value = '';
-        inputs.playerWord.className = '';
-        
-        messages.message.innerHTML = 'Getting Ready';
+        // Reset Game
+        resetGame();
         
         // Leave room
         socket.emit('leaveRoom');
         
         // Switch to disconnected frame
-        frame.switchTo(frame.dc);
+        frame.switchTo(frame.opponentLeft);
     });
     
-    button.frameDcGoHome.onclick = function() {
+    button.frameOpponentLeftGoHome.onclick = function() {
         // Switch to home frame
         frame.switchTo(frame.home);
     };
@@ -204,8 +196,11 @@ window.onload = function() {
         // New round
         round++;
         
-        // Remove classes
-        inputs.playerWord.className = '';
+        // Remove class(es) from input field
+        input.word.className = '';
+        
+        // Set focus on input field
+        input.word.focus();
         
         // Start countdown
         var counter = 7;
@@ -214,10 +209,10 @@ window.onload = function() {
             
             if (counter < 5 && counter > 0) {
                 // Display countdown
-                messages.message.innerHTML = counter;
+                message.set(message.game, counter);
             } else if (counter === 0) {
                 // Clear countdown
-                messages.message.innerHTML = randomWord;
+                message.set(message.game, randomWord);
                 
                 // Enable keys
                 keysFlag = !keysFlag;
@@ -236,19 +231,19 @@ window.onload = function() {
         // Check if keys are enabled
         if (keysFlag) {
             if (e.keyCode == key.enter) {
-                if (inputs.playerWord.value.trim().length > 0) {
+                if (input.word.value.trim().length > 0) {
                     // Compare words
-                    if (word === inputs.playerWord.value) {
+                    if (word === input.word.value) {
                         // Remove classes
-                        inputs.playerWord.className = '';
+                        input.word.className = '';
                         
                         // Emit word to server
-                        socket.emit('gameShoot', round, inputs.playerWord.value);
+                        socket.emit('gameShoot', round, input.word.value);
                         
                         // Disable keys
                         keysFlag = !keysFlag;
                     } else {
-                        inputs.playerWord.className = 'player__word--error';
+                        input.word.className = 'player__word--error';
                     }
                 }
             }
@@ -257,14 +252,48 @@ window.onload = function() {
     
     socket.on('roundWinner', function(roundResult) {
         // Show who won
-        roundResult ? messages.message.innerHTML = 'You Won!' : messages.message.innerHTML = 'You Lost!';
+        roundResult ? message.set(message.game, 'You Won!') : message.set(message.game, 'You Lost!')
         
         // Increase score
         roundResult ? score.won.dataset.duelsWon++ : score.lost.dataset.duelsLost++;
         
         // Clear input field
-        inputs.playerWord.value = '';
+        input.word.value = '';
         
         score.update();
+    });
+    
+    ///////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
+    //////
+    //////
+    //////      Shared functions / events
+    //////
+    //////
+    ///////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
+    
+    function resetGame() {
+        clearInterval(timer);
+        keysFlag = false;
+        round = 0;
+        word = null;
+        score.won.dataset.duelsWon = 0;
+        score.lost.dataset.duelsLost = 0;
+        score.update();
+        input.word.value = '';
+        input.word.className = '';
+        input.code.value = '';
+        
+        message.set(message.game, 'Getting Ready');
+    };
+    
+    socket.on('disconnect', function() {
+        frame.switchTo(frame.dc);
+        resetGame();
+    });
+
+    socket.on('connect', function() {
+        frame.switchTo(frame.home);
     });
 };
